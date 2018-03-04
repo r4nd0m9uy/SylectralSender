aa7ba8220aaf78013edf7d6dec7dd25bb104b352
Kriz De Logi <krizdelogi@MacBook-Air-van-Kriz.local>
2018-03-04 11:47:58 +0100

    Enhanced the str_replace code.

--- prepare_message.php
+++ prepare_message.php
@@ -4,39 +4,44 @@ $request=$_GET;
 $name=isset($request['name'])?$request['name']:"";
 $contact=preg_replace('/\D/', '', $request['contact']);
 $message=$request['text'];
-$date=isset($request['date'])?$request['date']:date('Y-m-d H:i:s');	
-	
-$message=str_replace('xzeacute', 'é', $message);
-$message=str_replace('xzegrave', 'è', $message);
-$message=str_replace('xzugrave', 'ù', $message);
-$message=str_replace('xzagrave', 'à', $message);
-$message=str_replace('xzecirc', 'ê', $message);
-$message=str_replace('xzocirc', 'ô', $message);
-$message=str_replace('xzccedil', 'ç', $message);
-$message=str_replace('xzicirc', 'î', $message);
-$message=str_replace('xzeuro', '€', $message);
-$message=str_replace('xzsquot', '\'', $message);
-$message=str_replace('xzexcla', '!', $message);
-$message=str_replace('xzinter', '?', $message);
-$message=str_replace('xzampersand', '&', $message);
-$message=str_replace('xzat', '@', $message);
-$message=str_replace('xzslash', '/', $message);
-$message=str_replace('xzbracopen', '(', $message);
-$message=str_replace('xzparagraph', '§', $message);
-$message=str_replace('xzbracclose', ')', $message);
-$message=str_replace('xzdollar', '$', $message);
-$message=str_replace('xzsemicolon', ';', $message);
-$message=str_replace('xzequal', '=', $message);
-$message=str_replace('xzcomma', ',', $message);
-$message=str_replace('xzplus', '+', $message);
-$message=str_replace('xzacirc', 'â', $message);
-$message=str_replace('xzdot', '.', $message);
-$message=str_replace('xzouml', 'ö', $message);
-$message=str_replace('xzuuml', 'ü', $message);
+$date=isset($request['date'])?$request['date']:date('Y-m-d H:i:s');
 
-var_dump(prepareMessage($contact, $name, $message, $date));
-var_dump($date);
-die('<b>Votre message:</b><br><i>'.$message.'</i><br><b>A été envoyé avec succès.</b>');
+$replaceArray = [
+'xzeacute' => 'é',
+'xzegrave' => 'è',
+'xzugrave' => 'ù',
+'xzagrave' => 'à',
+'xzecirc' => 'ê',
+'xzocirc' => 'ô',
+'xzccedil' => 'ç',
+'xzicirc' => 'î',
+'xzeuro' => '€',
+'xzsquot' => '\'',
+'xzexcla' => '!',
+'xzinter' => '?',
+'xzampersand' => '&',
+'xzat' => '@',
+'xzslash' => '/',
+'xzbracopen' => '(',
+'xzparagraph' => '§',
+'xzbracclose' => ')',
+'xzdollar' => '$',
+'xzsemicolon' => ';',
+'xzequal' => '=',
+'xzcomma' => ',',
+'xzplus' => '+',
+'xzacirc' => 'â',
+'xzdot' => '.',
+'xzouml' => 'ö',
+'xzuuml' => 'ü',
+];
+
+$message = str_replace(array_keys($replaceArray), array_values($replaceArray), $message)
+
+//var_dump(prepareMessage($contact, $name, $message, $date));
+//var_dump($date);
+echo '<b>Votre message:</b><br><i>'.$message.'</i><br><b>A été envoyé avec succès.</b>';
+exit();
 //die('message envoy&eacute;<script>window.open(\'\', \'_self\', \'\'); window.close();</script>');
 
 ?>
