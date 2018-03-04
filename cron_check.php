0aa903a2e5fed0430ac9c1570ba30432aa700e73
Kriz De Logi <krizdelogi@MacBook-Air-van-Kriz.local>
2018-03-04 15:05:42 +0100

    Added a var_dump to dump the sent message.

--- cron_check.php
+++ cron_check.php
@@ -6,10 +6,10 @@ This file must check if there are messages to be sent form the 'prepared_texts' 
 If so, they must be sent and transferd to the 'texts' table by using the transferPreparedMessagetoSentMessages function.
 */
 
-if($_SERVER['REMOTE_ADDR']=='xxx.xxx.xxx.xxx') { //Check if correct ip accesses this page
-	
+if($_SERVER['REMOTE_ADDR']=='xxx.xxx.xxx.xxx') {
+
 	require('database_connection/interaction.php');
-	
+
 	$tosend = getPreparedMessagesYetToBeSent();
 	if(!$tosend) {
 		echo "<html><head><meta http-equiv='refresh' content='30'></head><body><b>AUTOSEND:</b> ";
@@ -23,31 +23,31 @@ if($_SERVER['REMOTE_ADDR']=='xxx.xxx.xxx.xxx') { //Check if correct ip accesses 
 			$contact=preg_replace('/\D/', '', $contact);
 			$contact_name=$prepMessage['contact_name'];
 			$text=$prepMessage['text'];
-		
-		
+
+
 		//SEND THE MESSAGE
 			// Your Account Sid and Auth Token from twilio.com/user/account
-			$sid = "xxxxx";
-			$token = "xxxxx";
+			$sid = "xxxxxxx";
+			$token = "xxxxxxx";
 			$client = new Services_Twilio($sid, $token);
 
 			$message = $client->account->messages->create(array(
-    		"From" => 'xxxxxxxxxxxx', // From a valid Twilio number
+    		"From" => 'xxxxxxx', // From a valid Twilio number
     		"To" => $contact,   // Text this number
     		"Body" => $text,
 			));
-			
+
 		//TRANSFER THE MESSAGE DETAILS TO THE 'TEXTS' TABLE
-			transferPreparedMessagetoSentMessages($prepMessage['id']);
-			
+			var_dump(transferPreparedMessagetoSentMessages($prepMessage['id']));
+
 	}
-	
+
 	echo "<html><head><meta http-equiv='refresh' content='30'></head><body><b>AUTOSEND:</b> ";
     echo '<br/>Last connection: '.date('Y-m-d G:i:s');
 	echo "<br />SENT</body></html>";
-	
-	
-	
+
+
+
 }else{
 	header("HTTP/1.1 403 Forbidden" );
 	echo "<h1>HTTP/1.1 403 Forbidden</h1><hr />No Access. Your IP adress is: ".$_SERVER['REMOTE_ADDR'];
